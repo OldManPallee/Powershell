799a7eed16c631be0f53eccfb8518819e9123ac4
OldManPallee <121796235+OldManPallee@users.noreply.github.com>
2023-01-02 11:21:54 +0100

    Update email csv script final.ps1
    removed personal data

--- email csv script final.ps1
+++ email csv script final.ps1
@@ -1,9 +1,9 @@
 ﻿#Import the file that store Name email and codes
 #Created by Algirdas Palenskis.
-$data = import-csv "C:\Users\vx16\Desktop\finallist.csv"
+$data = import-csv "C:\Users\user1\Desktop\finallist.csv"
 
 #Declare From
-$from = "Johan Sjoberg <johan.sjoberg@starstable.com>"
+$from = "name <name.lastname@domain.com>"
 
 #Declare email content
 $email = $data.Email | select -unique
@@ -23,13 +23,6 @@ ForEach ($item in $data) {
     $Subject = "Merry Christmas"
     $Body = "Dear {0}" -f $item.User + "
 
-`nIt has been quite a roller coaster of a year for us here at Star Stable Entertainment. We've made a lot of amazing content (those who missed the brilliant 2022 IN REVIEW find it in Drive) and faced some significant challenges. 
-`nEverything we've done, we've done it together, like one big group of Soul Riders, and I look forward to continuing to do that next year. 
-`nI hope that you have a great time off, get the opportunity to recharge, perhaps hang out with Snoble, and if you miss the snow, know that our players are enjoying it in Jorvik! 
-`nAttached is a way to enhance that time off even further - a small (but as large as the Swedish Tax Authorities allow) sign of appreciation and celebration of the holiday spirit. 
-`nSpend it wisely and I'll see you in 2023!" +
-"`n`nGratefully yours, " +
-"`nJohan" +
 
 "`n`nHere is the giftcard for Webhallen {0}" -f $item.Code  + 
 
